533d4fa164a738d6646e2a785d6875af85e3c7b8
ishanat <143204735+ishanat@users.noreply.github.com>
2025-11-21 18:54:12 -0500

    uncomment failing tests

--- src/aws_modules/registry_api.py
+++ src/aws_modules/registry_api.py
@@ -377,15 +377,15 @@ def ingest_artifact(art_type, payload):
     if failing_metrics:
         logger.info(f"Package rejected due to insufficient scores: {failing_metrics}")
 
-        # --- IMPORTANT: TO TEST UPLOAD DESPITE LOW SCORES, 
+        # --- IMPORTANT: TO TEST UPLOAD DESPITE LOW SCORES,
         # COMMENT OUT THE RETURN BELOW ---
-        # return make_response(
-        #     424,
-        #     {
-        #         "error": "Package is not uploaded due to insufficient quality metrics",
-        #         "failing_metrics": failing_metrics,
-        #     },
-        # )
+        return make_response(
+            424,
+            {
+                "error": "Package is not uploaded due to insufficient quality metrics",
+                "failing_metrics": failing_metrics,
+            },
+        )
         # ----------------------------------------------------------------------------------
 
     logger.info("All non-latency metrics passed threshold, proceeding with ingestion")
