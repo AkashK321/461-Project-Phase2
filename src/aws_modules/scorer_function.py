565207d39e43195e296a348d98d642f70d7e4aa7
ishanat <143204735+ishanat@users.noreply.github.com>
2025-12-09 20:30:52 -0500

    comment out code quality

--- src/aws_modules/scorer_function.py
+++ src/aws_modules/scorer_function.py
@@ -67,7 +67,7 @@ def score_url(url: str, url_type: str) -> dict:
     if url_type == "code":
         tasks["bus_factor"] = lambda: get_bus_factor(url, url_type)
         # tasks["ramp_up_time"] = lambda: get_ramp_up(url, url_type)
-        tasks["code_quality"] = lambda: get_code_quality(url, url_type)
+        #tasks["code_quality"] = lambda: get_code_quality(url, url_type)
         tasks["license"] = lambda: get_license_score(url, url_type)
 
     elif url_type == "dataset":
