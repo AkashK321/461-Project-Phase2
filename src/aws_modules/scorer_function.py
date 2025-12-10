f04500cd3cd8f2b8b44f34c03a5785d54207c517
ishanat <143204735+ishanat@users.noreply.github.com>
2025-12-10 01:23:05 -0500

    uncomment

--- src/aws_modules/scorer_function.py
+++ src/aws_modules/scorer_function.py
@@ -72,9 +72,9 @@ def score_url(url: str, url_type: str) -> dict:
 
     elif url_type == "dataset":
         tasks["dataset_quality"] = lambda: get_dataset_quality_score(url, url_type)
-        # tasks["dataset_and_code_score"] = lambda: get_dataset_and_code_score(
-        #     url, url_type
-        # )
+        tasks["dataset_and_code_score"] = lambda: get_dataset_and_code_score(
+            url, url_type
+        )
         # tasks["license"] = lambda: get_license_score(url, url_type)
 
     elif url_type == "model":
